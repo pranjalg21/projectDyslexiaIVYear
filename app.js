4ce9dc26de2d1033c2580f5627fb2a04e00720b9
pulkit_singhania <pulkitsinghania100@gmail.com>
2022-05-13 02:32:05 +0530

    Changed script for Heroku

--- app.js
+++ app.js
@@ -1087,5 +1087,5 @@ function countWords(sentence){
 const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-  console.log("Server is listening on port 3000");
+  console.log(`Server is listening on port ${port}`);
 });
