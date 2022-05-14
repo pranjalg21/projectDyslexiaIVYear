52eee3d8804ad325103d09320dd6548615eb7e30
pulkit_singhania <pulkitsinghania100@gmail.com>
2022-05-14 12:53:10 +0530

    Mongoose version update

--- app.js
+++ app.js
@@ -48,9 +48,9 @@ mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useFindAndModify", false);
-mongoose.set("useCreateIndex", true);
+// mongoose.set("useNewUrlParser", true);
+// mongoose.set("useFindAndModify", false);
+// mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
   fname: String,
